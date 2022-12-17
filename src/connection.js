efda5d07af633cb350367a6f7efabf0f4f393954
Ajay Kumar <hiajayy@gmail.com>
2022-12-18 00:47:04 +0530

    added env

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
-
+require('dotenv').config({ path: __dirname + "/.env" });
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '12345678',
-    database: 'express_crud'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 }).promise();
 
 module.exports = connection;
